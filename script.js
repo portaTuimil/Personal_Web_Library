329b448e46f7a02a8c86047be5c7be13ac0554a6
Pablo Porta <100687198+DarkProgrammerEs@users.noreply.github.com>
2023-08-22 11:08:45 +0200

    Project Finished!!

--- script.js
+++ script.js
@@ -24,8 +24,6 @@ document.onclick = function (e){
     }
 }
 
-
-
 //Logic 
 let Title = document.querySelector('#title');
 let Author = document.querySelector('#author');
@@ -33,18 +31,33 @@ let Pages = document.querySelector('#pages');
 let IsRead = document.querySelector('#read');
 let Form = document.querySelector('.form')
 let myLibrary = [];
+let storedNames = JSON.parse(localStorage.getItem('list'));
+myLibrary = storedNames || []   //Check for null
+let storedMaxID = JSON.parse(localStorage.getItem('ID'));
+if (storedMaxID == ''){
+    storedMaxID = 0;
+}
+let maxID = parseInt(storedMaxID)
 
 //Constructor
-function Book(title, author, pages, isRead){
+function Book(title, author, pages, isRead, id){
     this.title = title,
     this.author = author,
     this.pages = pages,
     this.isRead = isRead
+    this.id = maxID
+    maxID +=1
+    localStorage.setItem('ID', JSON.stringify(maxID));
+    storedMaxID = JSON.parse(localStorage.getItem('ID'));
 }
 
 //Add Books:
-function addBookToLibrary(Title, Author, Pages, IsRead){
-    myLibrary.push(new Book(Title, Author, Pages, IsRead))
+function addBookToLibrary(Title, Author, Pages, IsRead, id){
+    myLibrary.push(new Book(Title, Author, Pages, IsRead, id))
+    if(myLibrary.length !== 0){
+        localStorage.setItem('list', JSON.stringify(myLibrary));
+    }
+    storedNames = JSON.parse(localStorage.getItem('list'));
 }
 
 //Submit button:
@@ -55,18 +68,13 @@ Form.addEventListener('submit', (e)=>{
     authorVal = Author.value;
     pagesVal = Pages.value;
     isReadVal = IsRead.checked;
+    id = maxID;
 
     if (pagesVal === ""){
         pagesVal = "unknown"
     }
 
-    addBookToLibrary(titleVal, authorVal, pagesVal, isReadVal)
-
-    console.log(titleVal)
-    console.log(authorVal)
-    console.log(pagesVal)
-    console.log(isReadVal)
-    console.log(myLibrary)
+    addBookToLibrary(titleVal, authorVal, pagesVal, isReadVal, id)
 
     addMenu.setAttribute(`style`, `display: none;`);
     menu = false;
@@ -89,7 +97,11 @@ function createBook(){
 
     //Creates a div and gives a class:
     let div = document.createElement('div')
-    div.classList.add('book')
+    div.classList.add('book');
+    div.classList.add('dropzone');
+    div.classList.add(book.id);
+    div.setAttribute('draggable', 'true');
+    div.setAttribute('ondragstart', `event.dataTransfer.setData('text/plain',null)`);
 
     //Creates title:
 
@@ -118,6 +130,7 @@ function createBook(){
     //Delete Sistem:
     buttonDelete.addEventListener('click', ()=>{
         myLibrary.splice(myLibrary.indexOf(book),1);
+        localStorage.setItem('list', JSON.stringify(myLibrary));
         createBook();
     })
     
@@ -134,6 +147,7 @@ function createBook(){
         } else{
             book.isRead = true;
         }
+        localStorage.setItem('list', JSON.stringify(myLibrary));
         createBook();
     })
 
@@ -148,8 +162,65 @@ function createBook(){
     div.appendChild(buttonRead)
 }}
 
+//Change Order:
+
+function changeOrder(from, to) {
+    let firstID = findIndexForID(from);
+    let secondID = findIndexForID(to);
+    let first = myLibrary[firstID]
+    let second = myLibrary[secondID]
+    myLibrary[firstID] = second;
+    myLibrary[secondID] = first;
+    createBook()
+}
+
+function findIndexForID(ID){
+    let result;
+        for (let i = 0; i < myLibrary.length; i++){
+            if (myLibrary[i].id == ID){
+                result = i;
+            }
+        }
+    return result;
+}
+
+//Drag:
+var dragged;
+
+document.addEventListener("drag", function (event) {
+}, false);
+
+document.addEventListener("dragstart", function (event) {
+    dragged = event.target;
+    event.target.style.opacity = .5;
+}, false);
+
+document.addEventListener("dragend", function (event) {
+    event.target.style.opacity = "";
+}, false);
+
+document.addEventListener("dragover", function (event) {
+    event.preventDefault();
+}, false);
+
+let firsElClasses, secondElClasses, firstID, secondID;
+document.addEventListener("dragenter", function (event) {
+    if (event.target != this && event.target.className.includes("dropzone")) {
+            firsElClasses = dragged.classList.value;
+            secondElClasses = event.target.classList.value;
+            firstID = firsElClasses.slice(14);
+            secondID = secondElClasses.slice(14);
+    }
+}, false);
+
+document.addEventListener("drop", function (event) {
+    changeOrder(firstID, secondID);
+    event.preventDefault();
+}, false);
 
 // Test:
-addBookToLibrary('The Pracmatic Programmer', 'Andy Hunt and Dave Thomas', '572', true)
-addBookToLibrary('Línea de Fuego', 'Arturo Pérez Reverte', '480', false)
+if(myLibrary.length == 0){
+    addBookToLibrary('The Pracmatic Programmer', 'Andy Hunt and Dave Thomas', '572', true);
+    addBookToLibrary('Línea de Fuego', 'Arturo Pérez Reverte', '480', false);
+}
 createBook()
